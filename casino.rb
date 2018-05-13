dd01abaf9f754527cd09488917cd360409b846cf
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-13 11:57:59 -0600

    Gemfile, coin update, and a horse game

--- casino.rb
+++ casino.rb
@@ -1,21 +1,50 @@
 require 'pry'
 require_relative 'high_low'
 require_relative 'coin'
+<<<<<<< HEAD
 require_relative 'guessing_game'
+=======
+require_relative 'horse_race'
+
+class Player
+  attr_accessor :name, :age, :bankroll, :enter
+ 
+  def initialize
+    puts "What is your name?"
+    @name = gets.strip
+
+    puts "How old are you?"
+    @age = gets.to_i
+    if @age < 18
+      puts "Take a hike kid!"
+      Casino.new
+    end
+    puts "How much money do you have?"
+    @bankroll = gets.to_i
+  end
+end
+>>>>>>> Gemfile, coin update, and a horse game
 
 class Casino
   attr_accessor :name, :player
   def initialize
     puts "Welcome to the best Casino in Salt Lake"
+<<<<<<< HEAD
     @player = Player.new
+=======
+    @cats = Player.new
+>>>>>>> Gemfile, coin update, and a horse game
     enter
   end
 
   def enter
+    puts "You have $#{@cats.bankroll} in your wallet"
     puts "Casino Games"
     puts "1) High or Low"
+<<<<<<< HEAD
     puts "2) Coin Game"
     puts "3) Guessing Game"
+    puts "4) Horse Race"
     puts "Choose one"
     choice = gets.strip
     case choice
@@ -25,15 +54,21 @@ class Casino
         Coin.new(@player.name, @player.bankroll)
       when "3"
         Guessing.new(@player.name, @player.bankroll)
+      when "4"
+        Horserace.new(@player.name, @player.bankroll)
       else
         puts "Invalid entry please choose from menu"
         enter
+=======
+    
+>>>>>>> Gemfile, coin update, and a horse game
     end
     enter
   end
 
 end
 
+<<<<<<< HEAD
 class Player
   attr_accessor :name, :age, :bankroll, :enter
   def initialize
@@ -74,4 +109,6 @@ end
 #   end
 # end
 
+=======
+>>>>>>> Gemfile, coin update, and a horse game
 Casino.new
--- coin.rb
+++ coin.rb
@@ -4,25 +4,64 @@ class Coin
   def initialize(name,bank)
     @name = name
     @bank = bank
-    puts "Pick heads or tails (h/t)"
-    puts "hello  #{@name}"
+    game_menu
+  end
+  
+  def game_menu
+    puts "you have $#{@bank} in your wallet"
+    puts "Hello, #{@name} flip a coin to make some money"
+    puts "1: place a wager."
+    puts "2: retutn to casino."
+    choice = gets.to_i
+    
+    case choice
+    when 1
+    wager
+    when 2
+    to_casino
+    else
+    puts "not a valid option, choose an option from the menu"
+    game_menu
+    end
+  end
+  
+  def to_casino
+  end
+  
+  def wager
+  #@regexnumber = /^\d+(\.\d+)?$/
+  puts "Please place a wager"
+    @wager = gets.to_i
+    if @wager > @bank 
+    puts "#{@name}?! you don't have that much money, wager only what you have!"
+    wager
+  #   elsif @wager == @regexnumber 
+#     puts "not a valid entry"
+#     wager
+    else
     heads
+    end
   end
 
   def heads
+   puts "Pick heads or tails (heads/tails)"
     side = gets.strip.downcase
-    nickle = ["h","t"].sample
+    nickle = ["heads","tails"].sample
     puts nickle
    if side != nickle
-     @bank = @bank - 5
-     puts @bank
+     @bank = @bank - @wager
+     puts "That stinks it was #{nickle} you lost $#{@wager}"
+     puts "You now have $#{@bank} in your wallet."
+     puts ""
    elsif side == nickle
-    @bank = @bank + 5
-     puts @bank
+    @bank = @bank + @wager
+    puts "Sweet it WAS #{nickle} you won, I bet that was the easiest $#{@wager} you've ever made"
+     puts "You now have $#{@bank} in your wallet."
+     puts ""
    else
       puts "Not a valid coice "
       heads
    end
-   heads
+ game_menu
   end
 end
